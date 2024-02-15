9f38d718ff86152223ffcd8371d26e48f28dc970
cloudandrain <110593039+cloudandrain@users.noreply.github.com>
2024-02-15 17:01:03 +0700

    Add files via upload

---
+++ Security/Client-Side-Encryption/SpotlightLabProjects/SalesAnalytic_origin.py
@@ -0,0 +1,30 @@
+# ToDo-2 Import Python Modules and Classes
+
+
+# ----------------------------------------------------------------
+# Function Declarations - Main logic starts near bottom of script
+# ----------------------------------------------------------------
+
+# ToDo-3 Implement table-creation function
+
+# ToDo-8 Implement on-prem key retrieval functions
+
+
+# ----------------------
+# Main Application Logic
+# ----------------------
+
+# ToDo-1 - Identify AWS region and build Table 1
+
+
+# ToDo-4 - Load Table 1 with sales data
+
+
+# ToDo-5 - Create Table 2
+
+# ToDo-6 - Load Table2 with client-encrypted sales data using DynamoDB Encryption SDK and KMS Keys
+
+
+# ToDo-7 - Create Table3 and use the DynamoDB Encryption Client with a Wrapped Materials Provider
+
+# ToDo-9 - Use AWS Encryption SDK to protect local data
